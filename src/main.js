be51ceb62f00b68708758da7e796a678e9fd7d42
sogrey <suzhaoheng2006@163.com>
2021-10-18 00:23:15 +0800

    💖 deploy main branch.

--- src/main.js
+++ src/main.js
@@ -2,8 +2,6 @@ import Vue from 'vue'
 import App from './App.vue'
 import axios from 'axios'
 
-import './assets/css/normalize.min.css'
-import './assets/plugins/font-awesome-4.7.0/css/font-awesome.min.css'
 import './assets/css/style.css'
 
 Vue.config.productionTip = false
